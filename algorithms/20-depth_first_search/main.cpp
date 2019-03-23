871969bd326470fa4c31b7e0440d969d853d48d5
KennyBlanckaert <KennyBlanckaert@hotmail.com>
2019-03-23 12:59:57 +0100

    graph depth first search

---
+++ algorithms/20-depth_first_search/main.cpp
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+#include "graph.h"
+
+int main(int argc, char** argv) {
+
+    Graph<Direction::UNDIRECTED> graph(9);
+    graph.addConnection(0, 1);
+    graph.addConnection(1, 2);
+    graph.addConnection(1, 4);
+    graph.addConnection(2, 3);
+    graph.addConnection(2, 4);
+    graph.addConnection(2, 5);
+    graph.addConnection(5, 6);
+    graph.addConnection(5, 7);
+    graph.addConnection(6, 8);
+    graph.addConnection(7, 8);
+
+    graph.draw("graph.dot");
+
+    vector<int> result = graph.depthFirstSearch();
+    for (auto iter = result.begin(); iter != result.end(); iter++) {
+        cout << *iter << " ";
+    }
+
+    return 0;
+}
