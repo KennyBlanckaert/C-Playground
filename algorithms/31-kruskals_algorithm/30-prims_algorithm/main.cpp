31f8b7c1cd08ac4be4488a935a2791ef033f796e
KennyBlanckaert <KennyBlanckaert@hotmail.com>
2019-05-12 14:32:55 +0200

    Init class for prim & kruskal algorithms

---
+++ algorithms/31-kruskals_algorithm/30-prims_algorithm/main.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+#include "weighted_graph.h"
+
+int main(int argc, char** argv) {
+
+    Weighted_Graph<Direction::UNDIRECTED, int> graph(7);
+    graph.addConnection(0, 1, 2);
+    graph.addConnection(0, 2, 3);
+    graph.addConnection(0, 3, 3);
+    graph.addConnection(1, 3, 4);
+    graph.addConnection(1, 4, 3);
+    graph.addConnection(2, 5, 7);
+    graph.addConnection(3, 4, 1);
+    graph.addConnection(3, 5, 6);
+    graph.addConnection(4, 5, 8);
+    graph.addConnection(5, 6, 9);
+    graph.draw("graph.dot");
+
+    // graph.prim();
+
+    return 0;
+}
